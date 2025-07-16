950ed145e679f0fe22ef1f78f16faa333a5451cc
Bartlomiej Roszczypala <gucu112@hotmail.com>
2025-07-16 02:28:12 +0200

    Add phone call test

---
+++ BindingAttributeInheritance/Features/Phone.feature
@@ -0,0 +1,10 @@
+﻿@Phone
+Feature: Phone
+
+Simple phone which calls last number
+
+Scenario: Call the last number
+	Given the phone number 000000000
+	And the second phoner number 123456789
+	When the phone call is made
+	Then the phone call happened
---
+++ BindingAttributeInheritance/StepDefinitions/PhoneStepDefinitions.cs
@@ -0,0 +1,39 @@
+﻿namespace BindingAttributeInheritance.StepDefinitions
+{
+    [Binding]
+    public sealed class PhoneStepDefinitions : Steps
+    {
+        private bool _wasCalled = false;
+
+        [Given("the phone number {int}")]
+        public void GivenThePhoneNumber(string phoneNumber)
+        {
+            ScenarioContext["PhoneNumber"] = phoneNumber;
+        }
+
+        [Given("the second phoner number {int}")]
+        public void GivenTheSecondPhoneNumber(string phoneNumber)
+        {
+            GivenThePhoneNumber(phoneNumber);
+        }
+
+        [When("the phone call is made")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            var phoneNumber = (string)ScenarioContext["PhoneNumber"];
+            MakePhoneCall(phoneNumber);
+        }
+
+        [Then("the phone call happened")]
+        public void ThenTheResultShouldBe()
+        {
+            Assert.That(_wasCalled, Is.True);
+        }
+
+        private void MakePhoneCall(string phoneNumber)
+        {
+            _wasCalled = true;
+            Console.WriteLine($"Making a phone call to {phoneNumber}.");
+        }
+    }
+}
